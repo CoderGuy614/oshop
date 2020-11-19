5830ea44bfd15a4d7507864df0434d9aceb2ec23
CoderGuy614 <jlutz.110@gmail.com>
2020-11-19 10:53:29 -0500

    Add User Login with Firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,20 @@
+import { AngularFireAuth} from '@angular/fire/auth';
 import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/app'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
 
-  ngOnInit(): void {
+   }
+
+  login() {
+    this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
   }
 
 }
